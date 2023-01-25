ccfeb6a40ed49dc71d2cb907ddab82be7f812d81
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-01-25 19:09:50 -0300

    [feat] add wallet repository

---
+++ src/main/java/com/rafaelmgr12/digitalwallet/repository/WalletRepository.java
@@ -0,0 +1,11 @@
+package com.rafaelmgr12.digitalwallet.repository;
+
+
+import com.rafaelmgr12.digitalwallet.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+
+}
