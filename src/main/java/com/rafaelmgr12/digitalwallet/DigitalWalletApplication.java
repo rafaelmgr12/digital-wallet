497102e006c747d906061593b23093c13b8c93ad
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-01-25 17:50:34 -0300

    init project

---
+++ src/main/java/com/rafaelmgr12/digitalwallet/DigitalWalletApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelmgr12.digitalwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalWalletApplication.class, args);
+    }
+
+}
