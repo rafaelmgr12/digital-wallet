2a8bfba242b7c2ec5f7f86a704893c9fee4fdac4
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-01-28 18:57:50 -0300

    [feat] add Wallet Controller

---
+++ src/main/java/com/rafaelmgr12/digitalwallet/controller/WalletController.java
@@ -0,0 +1,37 @@
+package com.rafaelmgr12.digitalwallet.controller;
+
+import com.rafaelmgr12.digitalwallet.dto.WalletDTO;
+import com.rafaelmgr12.digitalwallet.service.WalletService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.math.BigDecimal;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api")
+public class WalletController {
+
+    @Autowired
+    private WalletService walletService;
+
+    @GetMapping("wallets/{userId}")
+    public ResponseEntity<BigDecimal> getWallet(@PathVariable(value = "userId") String userId) {
+        BigDecimal amount = walletService.getBalance(userId);
+        return ResponseEntity.ok(amount);
+    }
+
+
+    @PostMapping("wallets/{userId}")
+    public ResponseEntity<WalletDTO> createWallet(@PathVariable(value = "userId") String userId,@RequestBody @Validated WalletDTO walletDTO, UriComponentsBuilder uriBuilder) {
+        WalletDTO wallet = walletService.createWalletByUserId(walletDTO, userId);
+        URI uri = uriBuilder.path("/wallets/{id}").buildAndExpand(wallet.getId()).toUri();
+        return ResponseEntity.created(uri).body(wallet);
+
+    }
+
+
+}
